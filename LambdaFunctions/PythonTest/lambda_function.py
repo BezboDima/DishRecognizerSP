4b06522eef8d8f485440943f791b62a032b9da0c
jcost108 <s1182350@monmouth.edu>
2023-10-09 11:16:29 -0400

    chatgpt links

--- LambdaFunctions/PythonTest/lambda_function.py
+++ LambdaFunctions/PythonTest/lambda_function.py
@@ -6,3 +6,23 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from CI/CD!')
     }
+
+# https://platform.openai.com/docs/api-reference/authentication
+# https://platform.openai.com/docs/api-reference/chat
+
+# To install the official Python bindings, run the following command:
+#   pip install openai
+# To install the official Node.js library, run the following command in your Node.js project directory:
+#   npm install openai@^4.0.0
+
+# example
+def callChatGPTAPI():
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
