7bc1662631709d563404ab01c80ddf5e36813765
BezboDima <dimabezbo@gmail.com>
2023-10-16 15:24:53 -0400

    layer test

--- LambdaFunctions/PythonTest/lambda_function.py
+++ LambdaFunctions/PythonTest/lambda_function.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from common_layer.common_module import custom_function
+from layers.common_layer.common_module import custom_function
 def lambda_handler(event, context):
     # TODO implement
     newText = custom_function()
