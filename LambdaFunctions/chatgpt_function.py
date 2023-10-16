b74caa4b7bd95545d4f2fe4a8957356b77e1b7de
acatapano <acatapano2002@verizon.net>
2023-10-15 20:04:14 -0400

    ChatGPT function added.

---
+++ LambdaFunctions/chatgpt_function.py
@@ -0,0 +1,15 @@
+import json
+import openai
+
+def lambda_handler(event, context):
+    # TODO implement
+    openai.api_key = "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"
+    prompt = str(event['prompt'])
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role":"user", "content":prompt}])
+    
+    print(completion.choices[0].message.content)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Result: ' + completion.choices[0].message.content)
+    }
