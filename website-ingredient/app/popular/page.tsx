ecc892a39abfaee1b3e15d1ada531700d90d60a3
jcost108 <s1182350@monmouth.edu>
2023-11-29 09:43:09 -0500

    added link to popular dishes

--- website-ingredient/app/popular/page.tsx
+++ website-ingredient/app/popular/page.tsx
@@ -67,97 +67,25 @@ export default function PopularDishes() {
 		
 		<div className="gap-2 grid grid-cols-4 grid-rows-2">
 			{list.map((item, index) => (
-				<Card className="w-full h-[300px]" shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
-					<CardBody className="overflow-visible p-0">
-						<Image
-						shadow="sm"
-						radius="lg"
-						width="100%"
-						alt={item.title}
-						className="w-full object-cover h-[250px]"
-						src={item.img}
-						/>
-					</CardBody>
-					<CardFooter className="text-white text-large justify-center">
-						<b>{item.title}</b>
-					</CardFooter>
-				</Card>
+				<Link href={item.img}>
+					<Card className="w-full h-[300px]" shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+						<CardBody className="overflow-visible p-0">
+							<Image
+							shadow="sm"
+							radius="lg"
+							width="100%"
+							alt={item.title}
+							className="w-full object-cover h-[250px]"
+							src={item.img}
+							/>
+						</CardBody>
+						<CardFooter className="text-white text-large justify-center">
+							<b>{item.title}</b>
+						</CardFooter>
+					</Card>
+				</Link>
 			))}
     	</div>
 	</div>
-
-	// 	<div className="gap-2 grid grid-cols-3 grid-rows-2 px-1">
-	// 		<Card className="w-full h-[300px] shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")">
-	// 			<CardBody className="overflow-visible p-0">
-	// 				<Image
-	// 					shadow="sm"
-	// 					radius="lg"
-	// 					width="100%"
-	// 					alt={item.title}
-	// 					className="w-full object-cover h-[140px]"
-	// 					src={item.img}
-	// 				/>
-	// 			</CardBody>
-	// 			<CardFooter className="text-white text-large font-bold justify-center">
-	// 				<p>Soup</p>
-	// 			</CardFooter>
-	// 		</Card>
-
-	// 		<Card className="w-full h-[300px]">
-	// 			<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-	// 				<p className="text-tiny text-white/60 uppercase font-bold">How It Works</p>
-	// 				<h4 className="text-white font-medium text-large">Made Possible by ChatGPT</h4>
-	// 			</CardHeader>
-	// 			<Image
-	// 				removeWrapper
-	// 				alt="Card background"
-	// 				className="z-0 w-full h-full object-cover"
-	// 				src="chatgpt.jpg"
-	// 			/>
-	// 		</Card>
-
-	// 		<Card className="w-full h-[300px]">
-	// 			<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-	// 				<p className="text-tiny text-white/60 uppercase font-bold">Backend Power</p>
-	// 				<h4 className="text-white font-medium text-large">Powered by AWS</h4>
-	// 			</CardHeader>
-	// 			<Image
-	// 				removeWrapper
-	// 				alt="Card background"
-	// 				className="z-0 w-full h-full object-cover"
-	// 				src="aws.jpg"
-	// 			/>
-	// 		</Card>
-			
-	// 		<Card className="w-full h-[300px]">
-	// 			<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-	// 				<p className="text-tiny text-white/60 uppercase font-bold">Beauty By Design</p>
-	// 				<h4 className="text-white font-medium text-large">Built using NextUI, Next.js and Tailwind CSS</h4>
-	// 			</CardHeader>
-	// 			<Image
-	// 				removeWrapper
-	// 				alt="Card background"
-	// 				className="z-0 w-full h-full object-cover"
-	// 				src="nextui.png"
-	// 			/>
-	// 		</Card>
-
-	// 		<Card className="h-[300px]">
-	// 		</Card>
-
-	// 		<Card className="w-full h-[300px]">
-	// 			<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-	// 				<p className="text-tiny text-white/60 uppercase font-bold">For More Information</p>
-	// 				<h4 className="text-white font-medium text-large">View the About page</h4>
-	// 			</CardHeader>
-	// 			<Image
-	// 				removeWrapper
-	// 				alt="Card background"
-	// 				className="z-0 w-full h-full object-cover"
-	// 				src="mu_logo.png"
-	// 			/>
-	// 		</Card>
-    // 	</div>
-	// </div>
 	);
 }
