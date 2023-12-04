15e78c3b5977dd330bb0b56e037699a0b40de966
BezboDima <dimabezbo@gmail.com>
2023-12-04 08:51:13 -0500

    APOOOOS

--- website-ingredient/app/popular/page.tsx
+++ website-ingredient/app/popular/page.tsx
@@ -60,7 +60,7 @@ export default function PopularDishes() {
 	<div>
 		<div>
 			<h1 className={title()}>Popular Dishes</h1>
-			<p className="py-2">Don't have a dish? Try uploading one of these!</p>
+			<p className="py-2">Don&apos;t have a dish? Try uploading one of these!</p>
 		</div>
 		
 		<br></br>
