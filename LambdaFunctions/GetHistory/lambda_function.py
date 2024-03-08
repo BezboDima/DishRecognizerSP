5c57f3e3d3402694975bf442be89c712db8d3c82
BezboDima <dimabezbo@gmail.com>
2024-03-08 11:48:57 -0500

    2 functions

---
+++ LambdaFunctions/GetHistory/lambda_function.py
@@ -0,0 +1,30 @@
+import boto3
+import hashlib
+import json
+def lambda_handler(event, context):
+    """Function returns the bool value depending if the user is registered or not in the
+    database given their login and password
+
+    :param login: email of the user
+    :param password: password of the user
+    :returns: Bool of whether the user was found or not
+    """
+    try:
+        #connect to the database
+        dynamodb = boto3.resource("dynamodb")
+        table_name = "LoginInfo"
+        table = dynamodb.Table(table_name)
+        
+        response = table.get_item(Key={'email':event['login']})
+        
+        item = response['Item']
+
+        # Replace 'your-attribute-name' with the attribute you want to retrieve
+        attribute_value = item.get('history')
+        return {'status' : True, 'history' : attribute_value}
+    except Exception as e:
+        print(e)
+        return {'status' : False, 'error': str(e)}
+if __name__ == '__main__':
+    event = {"login" : "bezbo", "password" : "bb"}
+    print(lambda_handler(event, None))
--- LambdaFunctions/s3Download/lambda_function.py
+++ LambdaFunctions/s3Download/lambda_function.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 import base64
 
-def upload_file(event, context):
+def download_file(event, context):
     """Upload a file to an S3 bucket
 
     :param file_name: File to upload
@@ -17,11 +17,16 @@ def upload_file(event, context):
     # Upload the file
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.put_object(
-            Body=image_bytes,
+        response = s3_client.get_object(
             Bucket=event["bucket"],
             Key=event["key"],
         )
-        return True
+        # Read the content of the response
+        image_content = response['Body'].read()
+
+        # Encode the binary data to Base64
+        base64_encoded_image = base64.b64encode(image_content).decode('utf-8')
+
+        return {'status' : True, 'image' : base64_encoded_image}
     except ClientError as e:
         return False
